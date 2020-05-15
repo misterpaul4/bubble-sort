d60e9cb714406872a9947f603a21273edbfe3d34
paul <ajuzepaul>
2020-05-15 22:47:33 +0300

    add bubble sort method with yield

--- b_sort.rb
+++ b_sort.rb
@@ -17,3 +17,23 @@ def bubble_sort(arr)
   end
   arr
 end
+
+def bubble_sort_by(arr)
+  to_ignore = arr.length - 1
+  swapped = nil
+  until swapped == false
+    swapped = nil
+    (0...to_ignore).each do |i|
+      if (yield arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    if swapped .nil?
+      swapped = false
+    else
+      to_ignore -= 1
+    end
+  end
+  arr
+end
